2494518faaee08d115baa493589570d6db855ed8
Yuri peña <yuri14111@gmail.com>
2024-08-27 20:45:55 -0500

    nt78 feat se crean variables de entorno para la bd

---
+++ app/database/configuration.py
@@ -0,0 +1,12 @@
+#datos para la conexion a BD
+
+dataBaseName="gestordb"
+userName="root"
+userPassword=""
+connectionPort=3306
+server="localhost"
+
+#creando la conexion
+dataBaseConnection=f"mysql+mysqlconnector://{userName}:{userPassword}@{server}:{connectionPort}/{dataBaseName}"
+
+print(dataBaseConnection)
---
+++ main.py
@@ -0,0 +1,5 @@
+nombre="Yuri"
+edad=36
+
+print("Mi nomobre es: ",nombre,"y tengo: ",edad,"años")
+print(f"Mi nombre es: {nombre} y tengo {edad} años")
